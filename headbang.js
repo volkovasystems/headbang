3e34f27e3aec82c60849b61133112c9b010a63f9
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-22 16:53:15 +0800

    Initial commit for headbang.js Added bang sub module.

---
+++ headbang.js
@@ -0,0 +1,40 @@
+/*:
+    @module-configuration:
+        {
+            "packageName": "headbang",
+            "fileName": "headbang.js",
+            "moduleName": "headbang",
+            "authorName": "Richeve S. Bebedor",
+            "authorEMail": "richeve.bebedor@gmail.com",
+            "repository": "git@github.com:volkovasystems/headbang.git",
+            "testCase": "headbang-test.js",
+            "isGlobal": true
+        }
+    @end-module-configuration
+
+    @module-documentation:
+
+    @end-module-documentation
+*/
+var headbang = function headbang( URL, responseHandler ){
+    /*:
+        @meta-configuration:
+            {
+                "URL:required": "string",
+                "responseHandler:required": "function"
+            }
+        @end-meta-configuration
+    */
+
+    bang( "HEAD", URL, function catcher( ){
+        if( this.responseState == 2 ){
+            if( this.status == 200 ){
+                responseHandler( );
+            }
+        }
+    } );
+};
+
+var bang = require( "./bang/bang.js" );
+
+( module || { } ).exports = headbang;
