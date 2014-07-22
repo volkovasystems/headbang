f20ff72bf581bb970de86c9508896f57481c58d8
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-07-23 00:17:35 +0800

    Completed the headbang procedure.

--- headbang.js
+++ headbang.js
@@ -1,40 +1,41 @@
 /*:
-    @module-configuration:
-        {
-            "packageName": "headbang",
-            "fileName": "headbang.js",
-            "moduleName": "headbang",
-            "authorName": "Richeve S. Bebedor",
-            "authorEMail": "richeve.bebedor@gmail.com",
-            "repository": "git@github.com:volkovasystems/headbang.git",
-            "testCase": "headbang-test.js",
-            "isGlobal": true
-        }
-    @end-module-configuration
+	@module-configuration:
+		{
+			"packageName": "headbang",
+			"fileName": "headbang.js",
+			"moduleName": "headbang",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/headbang.git",
+			"testCase": "headbang-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
 
-    @module-documentation:
+	@module-documentation:
 
-    @end-module-documentation
+	@end-module-documentation
 */
 var headbang = function headbang( URL, responseHandler ){
-    /*:
-        @meta-configuration:
-            {
-                "URL:required": "string",
-                "responseHandler:required": "function"
-            }
-        @end-meta-configuration
-    */
+	/*:
+		@meta-configuration:
+			{
+				"URL:required": "string",
+				"responseHandler:required": "function"
+			}
+		@end-meta-configuration
+	*/
 
-    bang( "HEAD", URL, function catcher( ){
-        if( this.responseState == 2 ){
-            if( this.status == 200 ){
-                responseHandler( );
-            }
-        }
-    } );
+	bang( "HEAD", URL, function catcher( ){
+		if( this.responseState == 2 ){
+			var headerString = this.getAllResponseHeaders( );
+			if( this.status == 200 ){
+				responseHandler( true, headerString );
+			}else if( this.status >= 400 ){
+				responseHandler( false, headerString );
+			}else{
+				responseHandler( null, headerString );
+			}
+		}
+	} );
 };
-
-var bang = require( "./bang/bang.js" );
-
-( module || { } ).exports = headbang;
