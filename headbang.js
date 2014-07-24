074d0eba50efcae0e3eb7c185eb2aa651c7cfbbd
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-24 14:39:50 +0800

    Added synchronous headbang.

--- headbang.js
+++ headbang.js
@@ -21,27 +21,48 @@ var headbang = function headbang( URL, responseHandler ){
 		@meta-configuration:
 			{
 				"URL:required": "string",
-				"responseHandler:required": "function"
+				"responseHandler:optional": "function"
 			}
 		@end-meta-configuration
 	*/
 
-	bang( "HEAD", URL, function catcher( error ){
+    if( typeof responseHandler == "function" ){
+        bang( "HEAD", URL, function catcher( error ){
 
-        if( this.readyState == 2 ||
-            this.readyState == 4 )
-        {
-			var headerString = this.getAllResponseHeaders( );
+            if( this.readyState == 2 ||
+                this.readyState == 4 )
+            {
+                var headerString = this.getAllResponseHeaders( );
 
-            if( error ){
-                responseHandler( error );
-            }else if( this.status == 200 ){
-				responseHandler( true, headerString );
-			}else if( this.status >= 400 ){
-				responseHandler( false, headerString );
-			}else{
-				responseHandler( null, headerString );
-			}
-		}
-	} );
+                if( error ){
+                    responseHandler( error );
+                }else if( this.status == 200 ){
+                    responseHandler( true, headerString );
+                }else if( this.status >= 400 ){
+                    responseHandler( false, headerString );
+                }else{
+                    responseHandler( null, headerString );
+                }
+            }
+        } );
+
+    }else{
+        var request = bang( "HEAD", URL );
+        var headerString = request.getAllResponseHeaders( );
+
+        var requestResult = {
+            "error": request.error,
+            "headerString": headerString
+        };
+
+        if( request.status == 200 ){
+            requestResult.status = true;
+        }else if( request.status >= 400 ){
+            requestResult.status = false;
+        }else{
+            requestResult.status = null;
+        }
+
+        return requestResult;
+    }
 };
